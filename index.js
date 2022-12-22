13e9f74790fcd90a67d0324eec47371024c85c49
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-12-22 14:00:48 +0100

    [Update]: added transition in sidebar toggle

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const toggleNavbar = document.querySelector("nav");
+const navBar = document.querySelector("nav");
+// const navItems = document.querySelector("nav ul");
 const btnMenuOpen = document.getElementById("menu-btn-open");
 const btnMenuClose = document.getElementById("menu-btn-close");
 
+// btnMenuOpen.addEventListener("click", () => {
+//     navBar.style.display = "block";
+// });
+
+// btnMenuClose.addEventListener("click", () => {
+//     navBar.style.display = "none";
+// });
+
+// navBar.style.display = "none";
+
+if (!navBar.classList.contains("open")) {
+    console.log(" close btn is not shown ------------- ");
+    btnMenuClose.style.display = "none";
+}
+
+
 btnMenuOpen.addEventListener("click", () => {
-    toggleNavbar.style.display = "block";
-});
+    console.log("Class name is added ++++++++++++");
+    navBar.classList.remove("close");
+    navBar.classList.add("open");
+
+    btnMenuClose.style.display = "block";
+
+})
 
 btnMenuClose.addEventListener("click", () => {
-    toggleNavbar.style.display = "none";
-});
+    if (navBar.classList.contains("open")) {
+        console.log("Class name is removed ------------- ");
+        navBar.classList.remove("open");
+        navBar.classList.add("close");
+        btnMenuClose.style.display = "none";
+
+    }
+})
