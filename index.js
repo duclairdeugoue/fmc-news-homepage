f42a74b11eeeee0fd6f23800fc45a787f18db0d7
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-12-22 14:22:04 +0100

    [Update]: refactoring js code and fixing desktop design

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 const navBar = document.querySelector("nav");
-// const navItems = document.querySelector("nav ul");
+const navItems = document.querySelector("nav ul");
 const btnMenuOpen = document.getElementById("menu-btn-open");
 const btnMenuClose = document.getElementById("menu-btn-close");
 
-// btnMenuOpen.addEventListener("click", () => {
-//     navBar.style.display = "block";
-// });
-
-// btnMenuClose.addEventListener("click", () => {
-//     navBar.style.display = "none";
-// });
-
-// navBar.style.display = "none";
+// Initial display
+navBar.classList.remove("open");
+navBar.classList.remove("close");
 
 if (!navBar.classList.contains("open")) {
     console.log(" close btn is not shown ------------- ");
     btnMenuClose.style.display = "none";
 }
 
-
 btnMenuOpen.addEventListener("click", () => {
-    console.log("Class name is added ++++++++++++");
+    console.log("class name is added ++++++++++++");
     navBar.classList.remove("close");
+    navItems.style.display = "block";
     navBar.classList.add("open");
 
     btnMenuClose.style.display = "block";
@@ -30,8 +24,9 @@ btnMenuOpen.addEventListener("click", () => {
 
 btnMenuClose.addEventListener("click", () => {
     if (navBar.classList.contains("open")) {
-        console.log("Class name is removed ------------- ");
+        console.log("class name is removed ------------- ");
         navBar.classList.remove("open");
+        navItems.style.display = "none";
         navBar.classList.add("close");
         btnMenuClose.style.display = "none";
 
